dc56dd60dce6e38fa181e3f62cdbd96309a57c1a
chaitanya <sparkscj234@gmail.com>
2022-10-09 17:53:10 +0530

    implemented using react-query

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Card from './components/card'
 import './styles/App.css'
+import { QueryClient,QueryClientProvider } from '@tanstack/react-query'
 
 const App = () => {
+  const client = new QueryClient()
   return (
+    <QueryClientProvider client={client}>
     <div className='container'>
       <h1 hidden>React practice</h1>
       <Card/>
     </div>
+    </QueryClientProvider>
   )
 }
 export default App
--- src/components/card.js
+++ src/components/card.js
@@ -1,41 +1,27 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
 import '../styles/card.css';
 import Divider from '../images/pattern-divider-mobile.svg';
 import Dice from '../images/icon-dice.svg'
 import { CircularProgress } from "@mui/material";
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
 const Card = () => {
-  const [Advice, setAdvice] = useState('')
-  const [Click, setClick] = useState(0)
-  const [isLoading,setLoading] = useState(false)
-  const url = 'https://api.adviceslip.com/advice';
+  const { data, isLoading, refetch } = useQuery(['advice'], () => {
+    return axios.get('https://api.adviceslip.com/advice').then(res => res.data.slip)
+  })
 
-  useEffect(() => {
-    setLoading(true)
-    const apiGet = async () => {
-      const response = await fetch(url)
-      const data = await response.json()
-      const slip = await data.slip
-      setAdvice(slip)
-      setLoading(false)
-    }
-    apiGet()
-  }, [Click])
 
   return (
     <div className="wrapper">
-      <p className="title">advice {'#' + Advice.id}</p>
+      <p className="title">advice {'#' + data?.id}</p>
       {
-        isLoading ? <CircularProgress /> : <q className="quote">
-          {Advice.advice}
-        </q>
+        isLoading ? <CircularProgress /> : <q className="quote">{data?.advice}</q>
       }
       <div>
         <img className="divider" src={Divider} alt="divider" />
       </div>
-      <button className="btn" title="generate-button"  onClick={() => setClick(!Click)}>
-        <img className="dice"  src={Dice} alt="" />
+      <button className="btn" title="generate-button" onClick={refetch}>
+        <img className="dice" src={Dice} alt="dice"/>
       </button>
     </div>
   );
